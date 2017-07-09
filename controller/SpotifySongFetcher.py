98c3b6c8f1ea889c0209c93f8d7035beecca9a62
sk8eddies <sk8eddie97@gmail.com>
2017-07-09 20:56:06 +0200

    TESTED that the spotifylib used to get the current song works and CREATED a main loop for the program.

--- controller/SpotifySongFetcher.py
+++ controller/SpotifySongFetcher.py
@@ -3,9 +3,10 @@ import spotilib
 
 class SpotifySongFetcher:
 
-    def __init__(self):
-        self._artist = spotilib.artist()
-        self._song = spotilib.song()
-        self._new_song = True
-        print("Artist: {0} || Song: {1}".format(self._artist, self._song))
-
+    # def fetch(self):
+    #     self._artist = spotilib.artist()
+    #     self._song = spotilib.song()
+    #     print("Artist: {0} || Song: {1}".format(self._artist, self._song))
+    #
+    # def is_new_song(self):
+    #     return not(spotilib.artist() == self._artist and spotilib.song() == self._song)
--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 from controller.SpotifySongFetcher import SpotifySongFetcher
+import time
 
 
 def main():
+    # Init parts of the controller
+    fetcher = SpotifySongFetcher()
+    # fetcher.fetch()
+
+    while(True):
+        # if fetcher.is_new_song():
+        #    fetcher.fetch()
+        time.sleep(3)
+
+main()
